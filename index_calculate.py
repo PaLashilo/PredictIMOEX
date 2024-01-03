4aeb5b1340f01d72253cfe0973c7ecb42ab67c13
PaLashilo <93659071+PaLashilo@users.noreply.github.com>
2024-01-03 22:17:56 +0300

    Add getting period boarders and init getting dataframes

--- index_calculate.py
+++ index_calculate.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from datetime import datetime
 import openpyxl
 import os
 
@@ -7,6 +8,7 @@ weights_file_path = 'Data\index_data\weights.xlsx'
 daily_data_folder_path = 'Data\index_data\daily_data'
 
 
+# get sheets with relevant wights
 def get_valid_sheet(file_path):
 
     workbook = openpyxl.load_workbook(file_path)
@@ -29,18 +31,38 @@ def get_valid_sheet(file_path):
 
     return sheets[:last_valid_sheet_index]
 
-def get_dateframe(folder_path):
+
+# get starts and ends of periods from sheets (end of last perioв is today or last parsed date)
+def get_period_borders_from_sheets(sheets):
+    dates = [datetime.strptime(date, '%d.%m.%Y').date() for date in sheets] # list of starts of calculation periods 
+    dates.insert(0, datetime.today().date()) # ?last parsed date
+    dates = dates[::-1]
+    return dates
+
+
+# form a dataframe with needed stock's price data
+def get_dateframe(folder_path, start, end):
     files = os.listdir(folder_path)
     for file in files:
         path =  os.path.join(folder_path, file)
         cur_df = pd.read_csv(path)
+        cur_df.waprice
         print(path)
         print(cur_df)
 
 
 
-sheets = get_valid_sheet(weights_file_path)
-get_dateframe(daily_data_folder_path)
+sheets = get_valid_sheet(weights_file_path) 
+period_borders = get_period_borders_from_sheets(sheets)
+
+print(period_borders)
+
+
+# check every period
+for i in range (len(period_borders)-1):
+    start_date = period_borders[i]
+    end_date = period_borders[i+1]
+    df = get_dateframe(daily_data_folder_path, start_date, end_date)
 
 
 
